9043db0a0cde6ee64e93ee2e9fc2cb5e7fa83a2a
Luiz Picol <luizpicolo@gmail.com>
2015-03-06 01:05:18 -0400

    Added param to assets papeline heroku

--- config/application.rb
+++ config/application.rb
@@ -43,6 +43,7 @@ module PersonalPage
    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
    config.i18n.default_locale = :'pt-BR'
+   onfig.serve_static_assets = true
 
    # Do not swallow errors in after_commit/after_rollback callbacks.
    config.active_record.raise_in_transactional_callbacks = true
