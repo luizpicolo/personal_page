665eb14bba67fde0647d0e9e4723c3f31d71ba05
Luiz Picol <luizpicolo@gmail.com>
2015-03-06 00:55:59 -0400

    removed gravatar

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,7 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find_by_slug(params[:slug])
 
-    @gravatar = Gravatar.new("luizpicolo@gmail.com").image_url
+    #@gravatar = Gravatar.new("luizpicolo@gmail.com").image_url
 
     @title = @article.title
     @description = @article.summary.first(255)
