90b65e40a1bb8b38a23b94a8f95adfde1fc1f522
Luiz Picolo <luizpicolo@gmail.com>
2015-08-24 00:08:47 -0400

    Added middleware Rack::Deflater

--- config/application.rb
+++ config/application.rb
@@ -42,6 +42,7 @@ module PersonalPage
 
    # Do not swallow errors in after_commit/after_rollback callbacks.
    config.active_record.raise_in_transactional_callbacks = true
+   config.middleware.use Rack::Deflater
 
    config.to_prepare do
     Devise::SessionsController.layout false
